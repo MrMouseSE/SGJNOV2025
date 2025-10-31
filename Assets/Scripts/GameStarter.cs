89810606762a9bd4829107d39c369566f2ff785e
Danil Bogodvid <d.bogodvid@kefirgames.com>
2025-10-31 10:59:47 +0300

    add: init commit with simple core structure

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+    public void Start()
+    {
+        InitGame();
+    }
+    
+    private void InitGame()
+    {
+        //TODO: Entrypoint code
+    }
+}
