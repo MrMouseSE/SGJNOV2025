10221db78e5d6623436ef7f471bc39fd336e27a1
IKoniashov <nintendocoin@yandex.ru>
2025-11-03 01:37:38 +0300

    Prefabs moved. Added player lef\right movement

---
+++ Assets/Scripts/BallScripts/BallDescription.cs
@@ -0,0 +1,8 @@
+namespace BallScripts
+{
+    [CreateAssetMenu(menuName = "Create LevelDescription", fileName = "LevelDescription", order = 0)]
+    public class BallDiscription
+    {
+        
+    }
+}
